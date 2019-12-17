e3ee77e940c06842ccf9e39effd4c652de6b45a6
TOSHIBA <TOSHIBA@TOSHIPC>
2019-12-17 07:52:57 -0500

    Proyecto de practica

---
+++ src/practicab/UsaPracticaB.java
@@ -0,0 +1,12 @@
+
+package practicab;
+
+
+public class UsaPracticaB {
+    public static void main(String[] args) {
+        Practica p=new Practica();
+        p.setVisible(true);
+        p.setLocationRelativeTo(null);
+        
+    }
+}
